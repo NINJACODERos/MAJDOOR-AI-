80cef9b1a3675ebb2401ac3df0625c2e3db3d925
NINJACODERos <findmexoox@gmail.com>
2026-07-16 11:41:21 +0530

    Change voice from SwaraNeural to MadhurNeural

--- file2.py
+++ file2.py
@@ -213,7 +213,7 @@ async def _edge_tts(text):
 
     communicate = edge_tts.Communicate(
         text=text,
-        voice="hi-IN-SwaraNeural"
+        voice="hi-IN-MadhurNeural"
     )
 
     await communicate.save(output)
