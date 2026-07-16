52af6a62863e42641fd8f556fbea357c35be0b3a
NINJACODERos <findmexoox@gmail.com>
2026-07-16 11:37:46 +0530

    Change voice to hi-IN-SwaraNeural for Hindi TTS

--- file2.py
+++ file2.py
@@ -213,7 +213,7 @@ async def _edge_tts(text):
 
     communicate = edge_tts.Communicate(
         text=text,
-        voice="en-US-AvaMultilingualNeural"
+        voice="hi-IN-SwaraNeural"
     )
 
     await communicate.save(output)
